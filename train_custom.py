d6f041aaa71b4e7384aaf55c6d966feb174f36e3
guerraquadrado@gmail.com <guerraquadrado@gmail.com>
2025-03-05 15:25:17 +0100

    version2

--- train_custom.py
+++ train_custom.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11m_custom.pt")
 
 # Train the model using the 'dataset_custom.yaml' dataset for x epochs
-model.train(data="dataset_custom.yaml", epochs=2, imgsz = 640, batch = 32, workers=3)
+model.train(data="dataset_custom.yaml", epochs=20, imgsz = 640, batch = 128, workers=7)

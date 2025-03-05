cece49a8843f016cdaf2d1736877417981396067
guerraquadrado@gmail.com <guerraquadrado@gmail.com>
2025-03-05 15:59:06 +0100

    val

--- validation.py
+++ validation.py
@@ -5,6 +5,3 @@ model = YOLO('version2.pt')
 
 # Évaluer le modèle sur l'ensemble de validation
 results = model.val(data='dataset_custom.yaml')
-
-# Afficher les métriques
-print(results.mean_results())  # Affiche les valeurs moyennes des métriques

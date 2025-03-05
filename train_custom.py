f6ecbf0a68468ab112ce618ba2ff7148958f4f4b
AlexMelissa <guerraquadrado@gmail.com>
2025-03-05 09:25:43 +0100

    pour juliet

---
+++ train_custom.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLO model (recommended for training)
+model = YOLO("yolo11m_custom.pt")
+
+# Train the model using the 'dataset_custom.yaml' dataset for x epochs
+model.train(data="dataset_custom.yaml", epochs=2, imgsz = 640, batch = 8, workers = 1, device = "cpu")

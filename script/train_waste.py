25d42e0de9b6506c77b4ef69b8fef749940888d6
guerraquadrado@gmail.com <guerraquadrado@gmail.com>
2025-04-10 15:56:50 +0200

    3eme version waste

--- script/train_waste.py
+++ script/train_waste.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11m_custom.pt")
 
 # Train the model using the 'dataset_custom.yaml' dataset for x epochs
-model.train(data="dataset_waste.yaml", epochs=100, imgsz = 640, batch = 64, workers=8)
+model.train(data="dataset_waste.yaml", epochs=50, imgsz = 640, batch = 64, workers=8)

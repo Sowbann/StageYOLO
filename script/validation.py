51d6ceffa0aebca849d98a45e66f65b07f3e4e72
AlexMelissa <guerraquadrado@gmail.com>
2025-05-13 16:04:43 +0200

    clean script

--- script/validation.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Charger votre modèle
-model = YOLO('version2.pt')
-
-# Évaluer le modèle sur l'ensemble de validation
-results = model.val(data='dataset_custom.yaml')
